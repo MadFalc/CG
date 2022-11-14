1ded16cb6e33d223dc33e33bb0549bc199ab32b4
MadFalc <68642371+MadFalc@users.noreply.github.com>
2022-11-14 14:17:12 +0100

    Feedbackmoment met de tech coach
    Tomi, kijk!

--- PHP uit syllabus/blok2.php
+++ PHP uit syllabus/blok2.php
@@ -1,9 +1,92 @@
 <head>
     <title>Blok 2!</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="img/heart icon.gif" type="image/gif"> <!--Kijk Tomi, als je hem opent in Firefox, dan pulseert het hartje!-->
+    <link rel="shortcut icon" href="img/heart icon.gif" type="image/gif">
+    <!--Kijk Tomi, als je hem opent in Firefox, dan pulseert het hartje!-->
 </head>
 
+<body>
+    <h1>BLOK 2</h1>
+    <h2>Opdracht: 1.1 - WHAT IS OOP?</h2>
+    <p>Leg in je eigen woorden uit wat het verschil is tussen flat PHP en Object Oriented Programming?<br> Let wel,
+        eigen woorden: Ik heb het idee dat de OOP op een vergelijkbare manier werkt als classes in CSS/html. Je maakt
+        een apart blokje/object waarin vanalles aangemaakt wordt en afspeelt. Vervolgens kun je dit object op
+        verschillende plekken vaker doen 'afspelen'.</p>
+    <p>Leg in je eigen woorden uit welke vier voordelen Object Oriented Programming heeft ten opzicht van flat PHP.<br>
+        VIER? 1. Herbruiken van toepassingen. 2. Overzicht creëren in je code. 3. Volgens W3 is het sneller en
+        makkelijker uit te voeren. 4. Niet alleen hoef je jezelf niet alsmaar te herhalen, maar dit zorgt er ook voor
+        dat de code makkelijker te de-buggen is.</p>
+    <p>Leg in je eigen woorden uit wat de twee hoofdonderdelen zijn van Object Oriented Programming?<br> Classes en
+        objects. Een class is dus wat ik met CSS vergeleek, een pakketje met allemaal bruikbare onderdelen. De object
+        zijn deze onderdelen. Dit kunnen variabelen met berekeningen zijn, of functies die vervolgens toegepast worden.
+        Enz enz.</p>
+    <p>Leg in je eigen woorden uit wat het verschil is tussen een Class en een Object?<br> Ik hoop dat ik deze vraag
+        duidelijk genoeg heb beantwoord hierboven.</p>
+    <p>Leg in je eigen woorden uit wat er overgeërfd wordt van een Class als een object aangemaakt wordt?<br> Volgens W3
+        erft een object alle properties en behaviors van de class, maar elk object heeft eigen values voor de
+        properties.</p>
+    <p>Bedenk zelf een voorbeeld van een Class en bijbehorende Objecten anders dan afgebeeld op W3SCHOOLS.<br> Class:
+        Games. Objects: Mario Sunshine, Bloodborne, Shadow of the Colossus en Doki Doki Literature Club.</p>
+    <br>
+    <h2>Opdracht: 1.2 - Classes and Objects</h2>
+    <p>Objecten erven dingen van elkaar over. Leg uit in je eigen woorden uit wat het verschil is tussen objecten?<br>
+        Je kunt meerdere objecten maken op basis van een class. Je kunt deze objecten andere namen geven dan elkaar en
+        dan de class. Vervolgens kun je de veriabelen/properties binnen je objecten aanpassen. Je verandert dan deze
+        objecten, maar de class blijft precies zoals hij was.</p>
+    <p>Wat is de meest basic Syntax definitie van een Class? Uit welke drie onderdelen bestaat deze Syntax?<br> class
+        className {Properties en methods}. Zo ongeveer. 1. Je roept aan dat je een class aanmaakt. 2. Deze krijgt een
+        naam waarmee je de class later weer op kan roepen/een object uit kan maken. 3. Properties en methods die in deze
+        class komen te zitten. </p>
+    <p>Hoe worden variables en functions in een Class genoemd?<br> Properties en methods.</p>
+    <p>Uit welke twee verschillende soorten fundamentele onderdelen kan de body van een Class bestaan?<br> Volgens mij
+        is het antwoord weer properties en methods.</p>
+    <p>Hoeveel objects kunnen er van een Class gemaakt worden?<br> Minstens 1.</p>
+    <p>Welke keyword gebruik je om je object mee aan te maken?<br> $naam = new className();. Keyword is new, dacht ik?
+    </p>
+    <p>Waar wordt het “$this” keyword voor gebruikt?<br> Wordt gebruikt IN methods om te refereren naar de huidige
+        object.</p>
+    <p>Hoe kan je een property van binnen in de Class veranderen versus buiten de klasse?<br> Binnen: $this->property =
+        nieuweValue; <br> Buiten: $objectName->property = nieuweValue;</p>
+    <p>Waar kun je de keyword instanceOf voor gebruiken?<br> instanceOf kun je gebruiken om te checken of een object
+        onderdeel is van een specifieke class.</p>
+    <br>
+    <h2>Opdracht: 1.3 - Constructor</h2>
+    <p>Leg uit waar een Constructor voor gebruikt wordt en wat het voordeel is?<br> </p>
+    <p>Hoe ziet de basic syntax van de Constructor eruit?</p>
+    <br>
+    <h2>Opdracht: 1.4 - Acces Modifiers</h2>
+    <p>Welke drie Access Modifier bestaan er?</p>
+    <p>Wat zijn hun verschillende rollen?</p>
+    <p>Op welke twee onderdelen van een Class kan je Access Modifiers gebruiken?</p>
+    <p>Wat denk je dat het nut is van Access Modifiers in een “echte” applicatie?</p>
+    <br>
+    <h2>Opdracht: 1.5 - Inheritance</h2>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <br>
+    <h2>Opdracht: 1.6 - Class Constants</h2>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <br>
+    <h2>Opdracht: 1.7 - Static Methods</h2>
+    <p></p>
+    <p></p>
+    <p></p>
+    <br>
+    <h2>Opdracht: 1.8 - Static Properties</h2>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <br>
+
+</body>
 
 <?php
 echo "Hey :)";
--- PHP uit syllabus/index.php
+++ PHP uit syllabus/index.php
@@ -1,7 +1,8 @@
 <head>
     <title>Main page!</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="img/heart icon.gif" type="image/gif"> <!--Kijk Tomi, als je hem opent in Firefox, dan pulseert het hartje!-->
+    <link rel="shortcut icon" href="img/heart icon.gif" type="image/gif">
+    <!--Kijk Tomi, als je hem opent in Firefox, dan pulseert het hartje!-->
 </head>
 
 <?php
